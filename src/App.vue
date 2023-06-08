e0da78aa076a7023a014695cd375f657a2fe0559
Shanover <shanoversaiyed@gmail.com>
2023-06-08 13:35:48 -0400

    Initial design

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template lang="">
-  <div>
-      <h1>Summify</h1>
+  <div style="align:center">
+      <!-- <h1>Summify</h1> -->
+
+      <div class="container p-3 text-center">
+        <img src="@/assets/logo.png" class="logo mt-5"/>
+        <h3>Summarize videos, time is precious</h3>
+        
+        <div class="p-4">
+          <URLFetcher/>
+        </div>
+      </div>
   </div>
 </template>
 <script>
+import URLFetcher from '@/components/URLFetcher.vue';
 export default {
-  
+    components:{
+      URLFetcher
+    }
 }
 </script>
-<style lang="">
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap');
+@import url("assets/mdb.min.css");
+@import url("assets/custom.css");
   
+  .logo{
+    max-height: 250px;
+  }
+
 </style>
---
+++ src/components/URLFetcher.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <input type="text" placeholder="Enter URL" class="form-control">
+        <button class="btn btn-primary btn-lg mt-4">Summify!</button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
